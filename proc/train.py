8168485717cc3dc12594671f5414be23cfe18309
Arash <arshemii1373.2@gmail.com>
2025-07-16 22:45:23 +0200

    Auto-commit on 2025-07-16 22:45:23

--- proc/train.py
+++ proc/train.py
@@ -102,6 +102,8 @@ class Trainer:
                 assert "label" in batch.keys()
                 loss = self.loss_fn(outputs, batch["label"], self.grid, oob_mask_valid)
                 
+            # TODO: must be removed
+            print(f"Loss value before scaling: {loss.item()}")
             scaler.scale(loss['total']).backward()
             scaler.step(self.optimizer)
             scaler.update()
