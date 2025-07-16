d4055dd6061bf77b7453654c229253780793ebd8
Arash <arshemii1373.2@gmail.com>
2025-07-16 22:46:16 +0200

    Auto-commit on 2025-07-16 22:46:16

--- proc/train.py
+++ proc/train.py
@@ -103,7 +103,7 @@ class Trainer:
                 loss = self.loss_fn(outputs, batch["label"], self.grid, oob_mask_valid)
                 
             # TODO: must be removed
-            print(f"Loss value before scaling: {loss.item()}")
+            print(f"Loss value before scaling: {loss}")
             scaler.scale(loss['total']).backward()
             scaler.step(self.optimizer)
             scaler.update()
