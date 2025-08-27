eb64f9e6ba841e6004bd967adeb1aa95f73e09b1
Arash Hashemi <arshemii1373.2@gmail.com>
2025-08-27 12:22:50 +0200

    Update metrics.py
    Adding the skeleton for BEV benchmarking

--- utils/metrics.py
+++ utils/metrics.py
@@ -15,3 +15,15 @@ class metrics_3d():
         
     def print_metrics(self, metrics):
         raise NotImplementedError("not implemented yet")
+
+
+
+class metrics_bev():
+    def __init__(self, cfg):
+        raise NotImplementedError("not implemented yet")
+        
+    def eval_from_prediction(self, eval_pair):
+        raise NotImplementedError("not implemented yet")
+        
+    def print_metrics(self, metrics):
+        raise NotImplementedError("not implemented yet")
