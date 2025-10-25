28076c182021913355ea9b5f58e9ad14b3014180
Arash <arshemii1373.2@gmail.com>
2025-10-25 22:26:11 +0200

    Auto-commit on 2025-10-25 22:26:11

--- utils/loss.py
+++ utils/loss.py
@@ -15,8 +15,8 @@ class loss3d(nn.Module):
         super(loss3d, self).__init__()
         self.cfg = cfg
         
-        self.grid = cfg.grid[0]
-        assert self.grid.shape[-1] == 3, f"Expected grid[..., 3] for (x,y,z), got shape {cfg.grid[0].shape}"
+        self.grid = cfg.grid_forward[0]
+        assert self.grid.shape[-1] == 3, f"Expected grid[..., 3] for (x,y,z), got shape {cfg.grid_forward[0].shape}"
         
         self.oob_mask_valid = cfg.oob_mask_valid[0] # inside FOV --> True
         self.num_c = self.cfg.model.num_class
