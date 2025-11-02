4e096b65b959ff127a3337b04b7342956910d73e
Arash <arshemii1373.2@gmail.com>
2025-11-02 22:30:42 +0100

    Auto-commit on 2025-11-02 22:30:42

--- modules/heads.py
+++ modules/heads.py
@@ -5,7 +5,7 @@ Note: all the inputs to these modules are in shape:
     (1, 259, 100, 30, 70)
     where we try to keep the spatial dimension the same since they are position clues
 """
-#import torch
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 #import math
@@ -65,47 +65,53 @@ class head_3d_detection(nn.Module):
         
 
     def forward(self, x, mode = 'eval', lw = [10.0, 6.0, 7.0, 1.8, 2.0, 0.1]):
-
-        out = self.conv1(x)  # in:1/4 out:1/8
-        pre = self.conv2(out)  # in:1/8 out:1/8
         
-        if self.debug:
-            print(f"first out shape is: {out.shape}")
-            print(f"first pre shape is: {pre.shape}")
-        
-        pre = F.relu(pre, inplace=True)
-
-        out = self.conv3(pre)  # in:1/8 out:1/16
-        if self.debug:
-            print(f"second out shape is: {out.shape}")
-        out = self.conv4(out)  # in:1/16 out:1/16
-        
-        if self.debug:
-            print(f"third out shape is: {out.shape}")
-            print(f"conv5(out) shape: {self.conv5(out).shape}")
-
-         
-        post = F.relu(self.conv5(out) + pre, inplace=True)
-
-        out = self.conv6(post)  # in:1/8 out:1/4
-        
-        if mode == 'train':
-            outputs = {}
-            lw = lw[ : -1]
-            for idx, w in enumerate(lw):
-                key = self.head_order[idx]
-                if w == 0:
-                    outputs[key] = None
-                else:
-                    outputs[key] = self.head_list[idx](out)
-            return outputs['obj'], outputs['dim'], outputs['cntr'], outputs['clss'], outputs['yaw']
-                    
-        else:
-            obj = self.head_list[0](out)
-            classes = self.head_list[1](out)
-            offset = self.head_list[2](out)
-            dims = self.head_list[3](out)
-            yaw = self.head_list[4](out)
-            return obj, dims, offset, classes, yaw
+        with torch.cuda.amp.autocast(enabled=False):
+            out = self.conv1(x)  # in:1/4 out:1/8
+            pre = self.conv2(out)  # in:1/8 out:1/8
+            
+            if self.debug:
+                print(f"first out shape is: {out.shape}")
+                print(f"first pre shape is: {pre.shape}")
+            
+            pre = F.relu(pre, inplace=True)
+    
+            out = self.conv3(pre)  # in:1/8 out:1/16
+            if self.debug:
+                print(f"second out shape is: {out.shape}")
+            out = self.conv4(out)  # in:1/16 out:1/16
+            
+            if self.debug:
+                print(f"third out shape is: {out.shape}")
+                print(f"conv5(out) shape: {self.conv5(out).shape}")
+    
+             
+            post = F.relu(self.conv5(out) + pre, inplace=True)
+    
+            out = self.conv6(post)  # in:1/8 out:1/4
+            
+            print(out.dtype)
+            for i, h in enumerate(self.head_list):
+                for n, p in h.named_parameters():
+                    print(f"Head {i}, {n} dtype: {p.dtype}")
+            
+            if mode == 'train':
+                outputs = {}
+                lw = lw[ : -1]
+                for idx, w in enumerate(lw):
+                    key = self.head_order[idx]
+                    if w == 0:
+                        outputs[key] = None
+                    else:
+                        outputs[key] = self.head_list[idx](out)
+                return outputs['obj'], outputs['dim'], outputs['cntr'], outputs['clss'], outputs['yaw']
+                        
+            else:
+                obj = self.head_list[0](out)
+                classes = self.head_list[1](out)
+                offset = self.head_list[2](out)
+                dims = self.head_list[3](out)
+                yaw = self.head_list[4](out)
+                return obj, dims, offset, classes, yaw
 
  
