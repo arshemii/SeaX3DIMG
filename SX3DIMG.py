03e40e73c122a102dde36db2465eb425d72322a5
Arash <arshemii1373.2@gmail.com>
2025-11-02 22:08:22 +0100

    Auto-commit on 2025-11-02 22:08:22

--- SX3DIMG.py
+++ SX3DIMG.py
@@ -190,7 +190,7 @@ class SX3DIMG(nn.Module):
         del full_voxel_flat
         return full_voxel
         
-    def forward(self, img_l, img_r, memory, create_memory = False, mode = 'eval'):
+    def forward(self, img_l, img_r, memory, create_memory = False, mode = 'eval', lw = [10.0, 6.0, 7.0, 1.8, 2.0, 0.1]):
         """
         img_l and img_r:    Shape (B, 3, 288, 960) --> h=288, w=960
         mem_left:           shape (B, 3, X, Y, Z)
@@ -233,7 +233,7 @@ class SX3DIMG(nn.Module):
         else:
             assert memory == None and create_memory == False
             voxel = self.conv_agg(voxel)  # reduce channels
-            out = self.head(voxel, mode) # 5 tensors
+            out = self.head(voxel, mode, lw) # 5 tensors
             if self.cfg.loss.aux_loss:
                 return out, disp_upsampled
             else:
