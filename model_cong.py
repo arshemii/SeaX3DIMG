29b29848d4a6a36ad76631673076b21cacdea8b8
Arash <arshemii1373.2@gmail.com>
2025-11-02 17:18:04 +0100

    Auto-commit on 2025-11-02 17:18:04

--- model_cong.py
+++ model_cong.py
@@ -214,12 +214,13 @@ def config_generator():
                             
                             "FINAL_CONV_KERNEL": 1}]
 
-
+    # order of weight:
+        # obj, cls, cntr, dim, yaw, disp
     cfg.loss.weight_debug = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
-    cfg.loss.weight = [8.0, 4.0, 6.0, 1.5, 1.0]
+    cfg.loss.weight = [10.0, 6.0, 7.0, 1.8, 2.0]
     cfg.loss.aux_loss = True
     if cfg.loss.aux_loss:
-        cfg.loss.weight.append(0.2)
+        cfg.loss.weight.append(0.1)
         cfg.model.return_disp = True
     cfg.loss.alpha = 0.55  # TODO: if model predicts a lot of objects, increase it 
     cfg.loss.gamma = 2.0
