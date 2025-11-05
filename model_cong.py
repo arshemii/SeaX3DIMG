cb74ea57b919f402fc6ea21c02b6aa88b7f05c75
Arash <arshemii1373.2@gmail.com>
2025-11-05 19:49:32 +0100

    Auto-commit on 2025-11-05 19:49:32

--- model_cong.py
+++ model_cong.py
@@ -12,8 +12,26 @@ Documentation:
         x--> -40 to +40
         y--> -0.64 to +3.86
         z --> +94
+------------------------------------------------------------------------------------------------------------
+Current values to test:
+    cfg.dev.scheduler = 'CAlr'
+    cfg.dev.num_epoch = 45
+    cfg.dev.t_max = cfg.dev.num_epoch
+    cfg.dev.eta_min = 5e-6
+    cfg.dev.lr = 6.5e-5
+    cfg.dev.weight_decay = 3e-5
 
-Experiments:
+
+------------------------------------------------------------------------------------------------------------
+Experiments by ranking for 45 epochs:
+
+config A5nov:
+    cfg.dev.scheduler = 'CAlr'
+    cfg.dev.num_epoch = 45
+    cfg.dev.t_max = cfg.dev.num_epoch
+    cfg.dev.eta_min = 1e-6
+    cfg.dev.lr = 8e-5
+    cfg.dev.weight_decay = 5e-5
 
 config A4nov:
     cfg.dev.scheduler = 'CAlr'
@@ -22,8 +40,6 @@ config A4nov:
     cfg.dev.eta_min = 1e-5
     cfg.dev.lr = 1.1e-4
     cfg.dev.weight_decay = 1e-4
-
-
 
 """
 
@@ -72,10 +88,10 @@ def config_generator():
     cfg.dev.scheduler = 'CAlr'  # options: 'OClr', 'CAlr', 'CAlrW'
     cfg.dev.num_epoch = 45
     cfg.dev.t_max = cfg.dev.num_epoch
-    cfg.dev.eta_min = 1e-6
+    cfg.dev.eta_min = 5e-6
     cfg.dev.mode = 'train'
-    cfg.dev.lr = 8e-5
-    cfg.dev.weight_decay = 5e-5
+    cfg.dev.lr = 6.5e-5
+    cfg.dev.weight_decay = 3e-5
     cfg.dev.eval_in_train = False
     cfg.dev.continue_training = False
     
