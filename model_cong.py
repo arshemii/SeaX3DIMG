619974af921da49eead43883c7fc7a8113958d12
Arash <arshemii1373.2@gmail.com>
2025-12-03 20:37:38 +0100

    Auto-commit on 2025-12-03 20:37:38

--- model_cong.py
+++ model_cong.py
@@ -110,7 +110,7 @@ def config_generator():
     cfg.model.conf_voxel = True
     cfg.model.sx3d.drop_out = 0.10
     cfg.model.sx3d.use_checkpoint = True
-    #cfg.model.sx3d.checkpoint_exp = './checkpoints_exp/staged_training/disp_obj_cls_cnt_dim45.pth'
+    cfg.model.sx3d.checkpoint_exp = './checkpoints_exp/staged_training/disp_obj_cls_cnt_dim45.pth'
     cfg.model.sx3d.checkpoint_3d = './checkpoints_3d/'
     cfg.model.sx3d.checkpoint_bev = './checkpoints_bev/'
     
