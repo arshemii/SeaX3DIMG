d0c1e61259f929ecd89aae8a831f3945836fecae
Arash <arshemii1373.2@gmail.com>
2025-12-31 12:11:18 +0100

    Auto-commit on 2025-12-31 12:11:18

--- model_cong.py
+++ model_cong.py
@@ -244,8 +244,8 @@ def config_generator():
     cfg.eval.save_dir_gt = './eval_dir_gt/'
     cfg.eval.split_eval = './dataset/sequential/val.txt'
     cfg.eval.score_th = 0.20
-    cfg.eval.batch_size = 2
-    cfg.eval.num_workers = 2
+    cfg.eval.batch_size = 4
+    cfg.eval.num_workers = 0
     cfg.eval.topk = 13
     cfg.eval.local_maxima_kernel = 9
     cfg.eval.estimate_2d = True
