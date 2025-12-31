c142af3158f2d3e59b7478f9f477852623576ed8
Arash <arshemii1373.2@gmail.com>
2025-12-31 14:01:17 +0100

    Auto-commit on 2025-12-31 14:01:17

--- proc/kitti-object-eval-python/eval.py
+++ proc/kitti-object-eval-python/eval.py
@@ -38,10 +38,14 @@ def get_thresholds(scores: np.ndarray, num_gt, num_sample_pts=41):
 
 
 def clean_data(gt_anno, dt_anno, current_class, difficulty):
-    CLASS_NAMES = [
-        'car', 'pedestrian', 'cyclist', 'van', 'person_sitting', 'car',
-        'tractor', 'trailer'
-    ]
+    # TODO: class names check
+    # CLASS_NAMES = [
+    #     'car', 'pedestrian', 'cyclist', 'van', 'person_sitting', 'car',
+    #     'tractor', 'trailer'
+    # ]
+    
+    CLASS_NAMES = ["Car", "Truck", "Person", "Cyclist"]
+    
     MIN_HEIGHT = [40, 25, 25]
     MAX_OCCLUSION = [0, 1, 2]
     MAX_TRUNCATION = [0.15, 0.3, 0.5]
